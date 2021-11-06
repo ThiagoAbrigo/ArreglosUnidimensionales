3dafd65a6f2cedb97e3b9d5fc57c3956bdd4d521
Home <Home@LAPTOP-A3A8UVMK>
2021-11-06 10:35:58 -0700

    Codigo fuente.

--- src/Model/Person.java
+++ src/Model/Person.java
@@ -23,7 +23,7 @@ public class Person {
         this.genero = genero;
         this.fechaNacimiento = fechaNacimiento;
         this.edad = edad;
-        //c
+        //cc
     }
     
     public String getName() {
